5f50bff2102084be54b93ea0b45582d3f21d4df6
Mausam Thapa <mausamwork@gmail.com>
2025-12-29 11:19:57 +0545

    Add Product model and update database connection to include it

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,9 +1,10 @@
 import { Sequelize } from "sequelize-typescript";
 import { envConfig } from "../config/config.js";
 import User from "./models/userModel.js";
+import Product from "./models/prductModel.js";
 
 const sequelize = new Sequelize(envConfig.connectionString as string, {
-    models: [User],
+    models: [User, Product],
 });
 
 // Test connection
@@ -12,7 +13,7 @@ const connectDB = async () => {
         await sequelize.authenticate();
         console.log("Database connected and authenticated successfully.");
 
-        await sequelize.sync({ force: false });
+        await sequelize.sync({ force: false, alter: true });
         console.log("Database and Model synced successfully.");
     } catch (error) {
         console.error("Database connection failed:", error);
---
+++ server/src/database/models/prductModel.ts
@@ -0,0 +1,51 @@
+import { Table, Column, Model, DataType, PrimaryKey, NotNull, AllowNull } from "sequelize-typescript"
+
+// Table is the class decorator for class Product
+@Table({
+    tableName: "products",
+    modelName: "Product",
+    timestamps: true
+})
+class Product extends Model {
+    @Column({
+        primaryKey: true,
+        type: DataType.UUID,
+        defaultValue: DataType.UUIDV4
+    })
+    declare productId: string
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    declare productTitle: string
+
+    @Column({
+        type: DataType.STRING
+    })
+    declare productDescription: string
+
+    @Column({
+        type: DataType.FLOAT,
+        allowNull: false
+    })
+    declare productPrice: number
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false
+    })
+    declare productStock: number
+
+    @Column({
+        type: DataType.INTEGER
+    })
+    declare productDiscount: number
+
+    @Column({
+        type: DataType.STRING,
+    })
+    declare productImageUrl: string
+}
+
+export default Product
