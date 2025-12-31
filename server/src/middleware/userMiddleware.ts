4dc0667ec1b8cb2ab2d73524503913ae89666dfc
Mausam Thapa <mausamwork@gmail.com>
2025-12-31 09:52:13 +0545

    feat: Modified user middleware to handle authorization (In Progress)

--- server/src/middleware/userMiddleware.ts
+++ server/src/middleware/userMiddleware.ts
@@ -2,9 +2,25 @@ import type { NextFunction, Request, Response } from "express";
 import sendResponse from "../services/sendResponse.js";
 import jwt from "jsonwebtoken"
 import { envConfig } from "../config/config.js";
+import User from "../database/models/userModel.js";
+
+export enum Role {
+    Customer = "customer",
+    Admin = "admin"
+}
+
+interface IExtendedRequest extends Request{
+    user?: {
+        id: string;
+        username: string;
+        email: string;
+        password: string;
+        role: string;
+    }
+}
 
 class UserMiddleware {
-    isUserLoggedIn(req: Request, res: Response, next: NextFunction) {
+    isUserLoggedIn(req: IExtendedRequest, res: Response, next: NextFunction) {
         // Take token from req.headers
         const token = req.headers.authorization
 
@@ -14,14 +30,31 @@ class UserMiddleware {
         }
 
         // validate token
-        jwt.verify(token, envConfig.jwtKey as string, (err, result) => {
+        jwt.verify(token, envConfig.jwtKey as string, async (err, result: any) => {
             if (err) {
                 sendResponse(res, 403, "Token is inavlid", err)
             } else {
-                console.log(result)
+                // console.log(result) -> { userID: , }
                 // req.userId = result.userId : Middleware allows to transfer req objects
+
+                // Find the user with result.userID
+                const userData = await User.findByPk(result.userId)
+
+                if (!userData) {
+                    sendResponse(res, 404, "User doesn't exist")
+                    return
+                }
+
+                req.user = userData
+
                 next() // After next never write success response
             }
+        })
+    }
+
+    restrictTo(...roles: Role[]) {
+        return ((req: IExtendedRequest, res: Response, next: NextFunction) => {
+            const userRole = req.user?.role as Role
         })
     }
 }
--- server/src/routes/categoryRoute.ts
+++ server/src/routes/categoryRoute.ts
@@ -4,7 +4,7 @@ import userMiddleware from "../middleware/userMiddleware.js"
 
 const router = express.Router()
 
-router.route("/").post(userMiddleware.isUserLoggedIn, categoryController.addCategory).get(categoryController.getCategories) // next allows to move between the routes
+router.route("/").post(userMiddleware.isUserLoggedIn, userMiddleware.restrictTo(), categoryController.addCategory).get(categoryController.getCategories) // next allows to move between the routes
 router.route("/:categoryId").delete(categoryController.deleteCategory).patch(categoryController.updateCategory)
 
 export default router
